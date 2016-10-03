56066cab221b26b7b89335f39007c3fc1aaf6d24
Andrey Kuznetsov <akuznetsov@reksoft.ru>
2016-10-03 12:11:38 +0300

    remove test-comments

--- src/main/java/es/ahs/hertest/controller/MailController.java
+++ src/main/java/es/ahs/hertest/controller/MailController.java
@@ -69,7 +69,7 @@ public class MailController {
             message.setSubject("Погода Testing Subject");
             /*message.setText("Dear Mail Crawler,"
                     + "\n\n mail2 No spam to my email, please!\n\n" + body);*/
-            message.setContent(body + "\n\n ---  Проверка русского в нативе", "text/html; charset=utf-8");
+            message.setContent(body, "text/html; charset=utf-8");
 //            message.setText(body, "utf-8", "html");
 //            message.setContent("<b>" + body + "</b>", "utf-8; text/html");
 
